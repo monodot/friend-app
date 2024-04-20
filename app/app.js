9b531595d6a1b95cf29fa4cc1d6463f98330da98
Tom Donohue <monodot@gmail.com>
2024-04-20 19:27:39 +0100

    Add Pouch DB, add friend and add note

--- app/app.js
+++ app/app.js
@@ -1,150 +1,164 @@
-// import getDbClient from './dbClient.js';
-
 "use strict";
-
-// Add the storage key as an app-wide constant
-const STORAGE_KEY = "friend-app-notes";
-const DATABASE_NAME = "friendapp";
-const FRIENDS_TABLE_NAME = "friends";
-const NOTES_TABLE_NAME = "notes";
-const DB_VERSION = 1;
 
 let state = {
   selectedView: { name: "home" },
 };
 
-let db;
+let db = new PouchDB('friends');
 
-const dbPromise = new Promise((resolve, reject) => {
-  const openRequest = indexedDB.open(DATABASE_NAME, DB_VERSION);
+const routes = [
+  {
+    pattern: 'contacts/add',
+    handler: renderAddContact,
+  },
+  { 
+    pattern: 'contacts/{id}', 
+    handler: renderContact, 
+  },
+  { 
+    pattern: 'contacts/{id}/notes/add', 
+    handler: renderContactAddNote,
+  },
+  { 
+    pattern: 'contacts/{id}/notes/{id}', 
+    handler: renderViewNote 
+  }
+]
+const defaultRouteHandler = renderHome;
 
-  openRequest.onupgradeneeded = function (e) {
-    // Perform database upgrade.
-    const db = e.target.result;
-    if (!db.objectStoreNames.contains(FRIENDS_TABLE_NAME)) {
-      const friends = db.createObjectStore(FRIENDS_TABLE_NAME, {
-        autoIncrement: true,
-      });
-      const notes = db.createObjectStore(NOTES_TABLE_NAME, {
-        autoIncrement: true,
-      });
-      notes.createIndex("name", "name", { unique: false });
 
-      friends.add({ name: "Alice" });
-      friends.add({ name: "Bob" });
-      friends.add({ name: "Charlie" });
-      friends.add({ name: "David" });
+async function navigateToView() {
+  let hash = window.location.hash.split("#");
+  console.log(hash);
+  let app = document.getElementById("app");
+  
+  // Default route
+  if (hash.length == 1) {
+    console.debug("Default route");
+    await defaultRouteHandler(app);
+    return true;
+  }
 
-      notes.add({
-        name: "Alice",
-        date: "2021-01-01",
-        type: "call",
-        text: "Called Alice",
-      });
-      notes.add({
-        name: "Alice",
-        date: "2021-01-02",
-        type: "meetup",
-        text: "Went for a drink at the cafe. Chatted about Hemingway.",
-      });
-      notes.add({
-        name: "Bob",
-        date: "2021-01-02",
-        type: "email",
-        text: "Emailed Bob",
-      });
-      notes.add({
-        name: "Charlie",
-        date: "2021-01-03",
-        type: "meeting",
-        text: "Met Charlie",
-      });
-      notes.add({
-        name: "David",
-        date: "2021-01-04",
-        type: "call",
-        text: "Called David",
-      });
+  for (let route of routes) {
+    let regexPattern = route.pattern.replace(/\{(\w+)\}/g, '(\\w+)'); // Convert placeholders to regex pattern
+    let regex = new RegExp('^' + regexPattern + '$'); // Create regex pattern for matching
+    let match = hash[1].match(regex);
+    if (match) {
+      let params = match.slice(1); // Extract variable values
+      console.log(`Params: ${params}`);
+      await route.handler(app, ...params); // Call route handler with extracted params
+      return true; // Route matched
     }
-  };
-
-  openRequest.onsuccess = function (e) {
-    db = e.target.result;
-    resolve(db);
-  };
-
-  openRequest.onerror = function (e) {
-    console.error("IndexedDB error:", e.target.errorCode);
-    reject(e.target.errorCode);
-  };
-});
-
-async function initData() {
-  try {
-    const circle = document.querySelector("friends-circle");
-    circle.friends = await getFriends();
-  } catch (error) {
-    console.error("Could not initialize database:", error);
-    // Handle failure: Maybe fallback to a different storage or notify the user
   }
 }
 
-initData();
+async function renderContact(app, params) {
+  const contact = await getContact(params);
+  if (!contact) {
+    console.log("Could not load contact");
+    return false;
+  }
+  const notes = await getContactNotes(params);
+  
+  app.innerHTML = `
+<div class="view">
+  <header class="title-with-left-link">
+    <p><a href="#" class="home-link">&lt; Friends</a></p>
+  </header>
+  <section id="contact-details">
+    <contact-details></contact-details>
+  </section>
+  <section id="contact-notes">
+    <a href="#contacts/${params}/notes/add">Add new note</a>
+    <notes-list></notes-list>
+  </section>
+</div>
+`;
 
-// create constants for the form and the form controls
-const recentNotesContainer = document.getElementById("recent-notes");
-const newNoteFormE = document.getElementsByTagName("form")[0];
-const noteContactSelectE = document.getElementById("note-contact");
-const noteDateInputE = document.getElementById("note-date");
-const noteTypeSelectE = document.getElementById("note-type");
-const noteTextInputE = document.getElementById("note-text");
-
-const canvasContainer = document.getElementById("canvas");
-
-// Listen to form submissions.
-newNoteFormE.addEventListener("submit", (event) => {
-  // Prevent the form from submitting to the server
-  // since everything is client-side.
-  event.preventDefault();
-
-  // Get the start and end dates from the form.
-  const noteDate = noteDateInputE.value;
-  const noteType = noteTypeSelectE.value;
-  const noteText = noteTextInputE.value;
-  const noteContact = noteContactSelectE.value;
-
-  // TODO: Do some validation here.
-
-  const note = {
-    contact: noteContact,
-    date: noteDate,
-    type: noteType,
-    text: noteText,
-  };
-
-  // Store the new note in our client-side storage.
-  storeNewNote(note);
-
-  // Refresh the UI.
-  const notesListElement = document.querySelector("notes-list");
-  notesListElement.updateComponent();
-
-  // Reset the form.
-  newNoteFormE.reset();
-
-  hidePopover();
-});
-
-function storeNewNote(note) {
-  // Get data from storage.
-  const notes = getAllNotes();
-
-  // Add the new note object to the end of the array of note objects.
-  notes.push(note);
-
-  // Store the updated array back in the storage.
-  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(notes));
+  const contactContainer = document.querySelector("contact-details");
+  contactContainer.name = contact.short_name;
+  const notesContainer = document.querySelector("notes-list");
+  notesContainer.notes = notes;
 }
+
+async function renderViewNote(params) {
+  console.log("HENLO");
+  // TODO
+}
+
+async function renderContactAddNote(app, params) {
+  app.innerHTML = `
+<div class="view">
+  <h2>Add note</h2>
+  <form id="new-note-form">
+    <p>Text: <input type="text" id="note-body"></p>
+    <p><button type="submit">Add note</button></p>
+  </form>
+</div>
+`;
+  const form = document.getElementById("new-note-form");
+  form.addEventListener("submit", (event) => {
+    const contactId = params; //  just the "contact_xxx" id 
+    const body = document.getElementById("note-body");
+    addNote(contactId, body.value)
+      .then(() => alert('note added!'))
+      .catch(() => alert('failed to add note'));
+    window.location.hash = `#contacts/${params}`;
+  });
+}
+
+async function renderAddContact(app, params) {
+  app.innerHTML = `
+<div class="view">
+  <h2>Add contact</h2>
+  <form id="new-contact-form">
+    <p>Name: <input type="text" id="contact-name"></p>
+    <p><button type="submit">Add contact</button></p>
+  </form>
+</div>
+`;
+  const form = document.getElementById("new-contact-form");
+  form.addEventListener("submit", (event) => {
+    // Prevent the form from submitting to the server
+    // since everything is client-side.
+    event.preventDefault();
+    const name = document.getElementById("contact-name");
+    addFriend(name.value)
+      .then(() => alert('added!'))
+      .catch(() => alert('failed!'));
+    window.location.hash = "#"; // go to home page
+  });
+}
+
+async function renderHome(app) {
+  console.log("ReNDERING HOME");
+  app.innerHTML = `
+<div class="view">
+  <header>
+    <h1>Friends</h1>
+  </header>
+  <p><a href="#contacts/add">Add friend</a></p>
+  <section id="inner-circle">
+    <friends-circle></friends-circle>
+  </section>
+</div>
+  `;
+  const circle = document.querySelector("friends-circle");
+  circle.friends = await getFriends();
+}
+
+window.addEventListener("hashchange", navigateToView);
+navigateToView()
+  .then(() => console.log("route matched"))
+  .catch((err) => console.log(`no route matched - ${err}`));
+
+
+
+db.changes({
+  since: 'now',
+  live: true
+}).on('change', navigateToView);
+
 
 function showError(message) {
   const errorElement = document.createElement("p");
@@ -164,130 +178,79 @@ function formatDate(dateString) {
   return date.toLocaleDateString("en-US", { timeZone: "UTC" });
 }
 
-async function navigateToView(target) {
-  const { name, params } = target;
-  // Hide all views
-  document.querySelectorAll("main > div.view").forEach((view) => {
-    view.style.display = "none";
-  });
-  switch (name) {
-    case "home":
-      state.selectedView = { name: "home" };
-      // document.querySelector("#home-view").classList.remove("hidden");
-      document.querySelector("#home-view").style.display = "block";
-      break;
-    case "new-note":
-      state.selectedView = { name: "new-note" };
-      // document.querySelector("#new-note-view").classList.remove("hidden");
-      document.querySelector("#new-note-view").style.display = "block";
-      break;
-    case "contact":
-      const notes = await getNotesForUser(params.name);
-      const contactDetails = document.querySelector("contact-details");
-      contactDetails.name = params.name; // update the displayed contact
-      const notesList = document.querySelector("notes-list");
-      notesList.notes = notes;
 
-      state.selectedView = { name: "contact", params };
-      document.querySelector("#contact-view").style.display = "block";
-      break;
-    default:
-      break;
-  }
-}
-
-function showPopover() {
-  document.querySelector("#edit-note-view").style.display = "block";
-}
-function hidePopover() {
-  document.querySelector("#edit-note-view").style.display = "none";
-}
 
 // STATE ----------------------------------------------
 
-async function getNotesForUser(name) {
+async function getContact(id) {
   try {
-    const db = await dbPromise;
-    const transaction = db.transaction(NOTES_TABLE_NAME, "readonly");
-    const store = transaction.objectStore(NOTES_TABLE_NAME);
-    // const index = store.index("name");
-    const request = store.index("name").getAll(name);
+    console.log(`Searching for contact ${id}`);
+    var result = await db.get(id);
+    // console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-    // Return a Promise so the caller can await it
-    return new Promise((resolve, reject) => {
-      request.onsuccess = function (event) {
-        const notes = event.target.result;
-        resolve(notes);
-      };
-      request.onerror = function (event) { //  THIS SEEMS WRONG
-        reject("Error getting notes");
-      };
-    });
-  } catch (error) {
-    console.error("Could not initialize database:", error);
-    showError("No database connection.");
+async function getContactNotes(contactId) {
+  try {
+    var notes = await db.allDocs({
+      include_docs: true,
+      startkey: `note_${contactId}`,
+      endkey: `note_${contactId}\ufff0`
+    })
+    return notes.rows;
+  } catch (err) {
+    console.error(err);
     return [];
   }
 }
 
 async function getFriends() {
   try {
-    const db = await dbPromise;
-    const transaction = db.transaction(FRIENDS_TABLE_NAME, "readonly");
-    const store = transaction.objectStore(FRIENDS_TABLE_NAME);
-    const request = store.getAll();
-
-    // Return a Promise so the caller can await it
-    return new Promise((resolve, reject) => {
-      request.onsuccess = function (event) {
-        const friends = event.target.result;
-        resolve(friends);
-      };
-      request.onerror = function (event) {
-        reject("Error getting friends");
-      };
+    var result = await db.allDocs({
+      include_docs: true, 
+      startkey: 'contact', // e.g. 'contact_1713622675061',
+      endkey: 'contact\ufff0' // e.g. 'contact_1713622675061'
     });
-  } catch (error) {
-    console.error("Could not initialize database:", error);
-    // Handle failure: Maybe fallback to a different storage or notify the user
-    showError("No database connection.");
-    return [];
+    return result.rows;
+  } catch (err) {
+    console.log(err);
   }
 }
+
+// To test in the web developer console:
+// friend = await addFriend("Sam");
+// friend.id;
+async function addFriend(name) {
+  const id = new Date().getTime();
+  try {
+    let result = await db.put({
+      _id: `contact_${id}`,
+      short_name: name,
+    });
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function addNote(contactId, body) {
+  const id = new Date().getTime();
+  try {
+    let result = await db.put({
+      _id: `note_${contactId}_${id}`,
+      body
+    });
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 
 // COMPONENTS ----------------------------------------------
-
-class RecentNotes extends HTMLElement {
-  set notes(value) {
-    this._notes = value;
-    this.updateComponent();
-  }
-
-  get notes() {
-    return this._notes;
-  }
-
-  connectedCallback() {
-    this.updateComponent();
-  }
-
-  updateComponent() {
-    if (!this.notes) {
-      return;
-    }
-    this.innerHTML = this.notes
-      .map(
-        (note) => `
-          <p><strong>${note.contact}</strong> <time>${formatDate(
-          note.date
-        )}</time> <em>${note.type}</em></p>
-            <p>${note.text}</p>
-        `
-      )
-      .join("");
-  }
-}
-customElements.define("recent-notes", RecentNotes);
 
 class FriendsCircle extends HTMLElement {
   set friends(value) {
@@ -303,17 +266,17 @@ class FriendsCircle extends HTMLElement {
     this.updateComponent();
   }
 
-  async updateComponent() {
+  updateComponent() {
     if (!this.friends) {
       return;
     }
     this.innerHTML = `<div class="friend-grid">${this.friends
       .map(
         (friend) => `<div>
-        <a href="#" onclick="navigateToView({ name: 'contact', params: { name: '${friend.name}' }}); return false;">
-          <svg viewBox="0 0 86 86" style="width:80%;"><ellipse style="fill:#FFD6AF;" cx="43" cy="43" rx="40" ry="40"></ellipse>
+        <a href="#contacts/${friend.id}">
+          <svg viewBox="0 0 86 86" style="width:80%;"><ellipse style="fill:#e2e8f0;" cx="43" cy="43" rx="40" ry="40"></ellipse>
           </svg>
-          <p style="margin:0.5rem 0">${friend.name}</p>
+          <p style="margin:0.5rem 0">${friend.doc.short_name}</p>
         </a></div>`
       )
       .join("")}</div>`;
@@ -361,16 +324,66 @@ class NotesList extends HTMLElement {
     if (!this.notes) {
       return;
     }
+    console.log(this.notes);
+    // <p><time>${formatDate(
+    //   note.doc.date
+    // )}</time> <em>${note.doc.type}</em></p>
     this.innerHTML = `<div class="notes-list">${this.notes
       .map(
         (note) => `<div>
-          <p><strong>${note.contact}</strong> <time>${formatDate(
-          note.date
-        )}</time> <em>${note.type}</em></p>
-            <p>${note.text}</p>
+            <p>${note.doc.body}</p>
         </div>`
       )
       .join("")}</div>`;
   }
 }
 customElements.define("notes-list", NotesList);
+
+
+class NewNoteForm extends HTMLElement {
+  set contactId(value) {
+    this._contactId = value;
+    this.updateComponent();
+  }
+
+  get contactId() {
+    return this._contactId;
+  }
+
+  connectedCallback() {
+    this.updateComponent();
+  }
+
+  updateComponent() {
+    if (!this.contactId) {
+      return;
+    }
+    this.innerHTML = `          <form>
+      <nav class="sheet-actions">
+        <a href="#" onclick="hidePopover(); return false;">Cancel</a>
+        <button type="submit">Save</button>
+      </nav>
+      <p>
+        <label for="date">Date</label>
+        <input type="date" id="note-date" required />
+      </p>
+      <p>
+        <label for="type">Type</label>
+        <select id="note-type" required>
+          <option value="">Select a type</option>
+          <option value="phone">Phone call</option>
+          <option value="meeting">Hang out</option>
+          <option value="update">Update</option>
+        </select>
+      </p>
+      <p>
+        <label for="note">Note</label>
+      </p>
+      </p>
+        <textarea id="note-text" required></textarea>
+      </p>
+      <input type="hidden" name="contactId"
+    </form>`;
+  }
+}
+customElements.define("new-note-form", NewNoteForm);
